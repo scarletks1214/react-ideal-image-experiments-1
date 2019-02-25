db964588e0f673d4d53dec68e35b01d44dbca767
scarlet.ks1214 <scarlet.ks1214@gmail.com>
2019-02-25 20:41:13 +0300

    added svg react component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import image3 from './images/vincent-van-zalinge-408523-unsplash.json'
 import image4 from './images/marvin-meyer-188676-unsplash.json'
 import image5 from './images/nidhin-mundackal-281287-unsplash.json'
 
+import { ReactComponent as IconComponent } from './icons/outline-autorenew-24px.svg';
+
 const Image = props => <IdealImage {...props} />
 
 Image.defaultProps = {
@@ -16,8 +18,6 @@ Image.defaultProps = {
 
 const src = ({name, ext, digest, width}) =>
   `./images/${name}-${width}.${digest}.${ext}`
-
-const customIcon = (<svg xmlns="http://www.w3.org/2000/svg" fill='#123456' width="92" height="92" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z"/></svg>);
 
 const props = obj => ({
   width: obj.width,
@@ -29,8 +29,8 @@ const props = obj => ({
   },
   srcSet: obj.sizes,
   icons: {
-    loading: (props) => customIcon,
-    load: (props) => customIcon,
+    loading: (props) => <IconComponent width={props.iconSize} height={props.iconSize} {...props} />,
+    load: (props) => <IconComponent width={props.iconSize} height={props.iconSize} {...props} />,
   },
   getMessage: (icon, state) => {
     switch (icon) {
@@ -48,7 +48,7 @@ const props = obj => ({
         return '';
     }
   },
-  iconSize: 92
+  iconColor: "#123456",
 })
 
 const App = () => (
