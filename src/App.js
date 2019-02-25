75a5d65933e260c6e084adc3ddd56257645b0c15
scarlet.ks1214 <scarlet.ks1214@gmail.com>
2019-02-25 20:00:49 +0300

    customize message and icon

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ Image.defaultProps = {
 const src = ({name, ext, digest, width}) =>
   `./images/${name}-${width}.${digest}.${ext}`
 
+const customIcon = (<svg xmlns="http://www.w3.org/2000/svg" fill='#123456' width="92" height="92" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z"/></svg>);
 
 const props = obj => ({
   width: obj.width,
@@ -27,24 +28,27 @@ const props = obj => ({
     return src({...obj, ext, width})
   },
   srcSet: obj.sizes,
-  getIcon: state => {
-    let loadState = state.loadState,
-    overThreshold = state.overThreshold;
-
-    if (typeof window === 'undefined' || window.navigator.userAgent === 'ReactSnap') return 'noicon';
-    switch (loadState) {
+  icons: {
+    loading: (props) => customIcon,
+    load: (props) => customIcon,
+  },
+  getMessage: (icon, state) => {
+    switch (icon) {
+      case 'load':
+        return 'download now';
       case 'loaded':
-        return 'loaded';
+        return 'loaded successfuly';
       case 'loading':
-        return overThreshold ? 'loading' : 'noicon';
-      case 'initial':
-        return 'loading';
+        return 'loading...';
+      case 'noicon':
+        return '';
       case 'error':
         return 'error'
       default:
-        throw new Error('Wrong state: ' + loadState);
+        return '';
     }
-  }
+  },
+  iconSize: 92
 })
 
 const App = () => (
